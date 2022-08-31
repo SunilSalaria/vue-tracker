8f03c39f3efd94e63d6235191178bd471b574130
Sunil Salaria <sunilsalaria1993@gmail.com>
2022-09-01 00:52:00 +0530

    login view

--- Tracker/src/views/IndexView.vue
+++ Tracker/src/views/IndexView.vue
@@ -1,19 +1,27 @@
 <template>
-    <section class="container-fluid">
-        <div class="row d-flex justify-content-center align-items-center vh-100 bg-primary text-center" id="login-vue">
+    <section class="container-fluid" id="index-vue">
+        <div class="row d-flex justify-content-center align-items-center vh-100 bg-primary text-center">
             <div class="col-12 col-md-8 col-lg-4">
-                <div class="display-6 mb-2 fw-bold text-white">Welcome to Tracker</div>
-                <p class="small mb-0 text-muted">We are offering a digital asset Tracker with maximum security and advanced features.
-                </p>
-                <div class="py-5 my-md-5">
-                    <img class="bg-white shadow-md rounded-circle p-3" src="../assets/tracker-index-logo.svg" height="200" alt="index-logo-img">
-                </div>
-                <div>
-                <div class="d-grid mb-3">
-                 <button type="button" class="btn btn-light text-primary btn-color shadow-sm fw-bold btn-lg">SIGN UP</button>
-                </div>                   
-                    <div class="text-muted">Already a member?</div>
-                    <a href="#" class="text-decoration-none fw-bold text-white">Sign In</a>
+                <div class="card shadow-lg bg-primary rounded-5">
+                    <div class="card-body p-md-5">
+                        <div class="display-6 mb-2 text-white">Welcome to Tracker</div>
+                        <p class="mb-0 text-muted">We are offering a digital asset Tracker with maximum security and
+                            advanced features.
+                        </p>
+                        <div class="py-5 my-md-5">
+                            <img class="bg-white shadow-lg rounded-circle p-3" src="../assets/tracker-index-logo.svg"
+                                height="200" alt="index-logo-img">
+                        </div>
+                        <div>
+                            <div class="d-grid mb-3">
+                                <button type="button"
+                                    class="btn btn-light text-primary shadow-lg btn-lg rounded-pill">Register an
+                                    Account</button>
+                            </div>
+                            <div class="text-muted">Already a member?</div>
+                            <a href="#" class="text-decoration-none text-white">Sign In</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -25,5 +33,4 @@
 
 
 <style>
-
 </style>
--- Tracker/src/views/LoginView.vue
+++ Tracker/src/views/LoginView.vue
@@ -1,9 +1,44 @@
 <template>
- <h1 class="text-success">Login View</h1>
+    <section class="container-fluid" id="login-vue">
+        <div class="row d-flex justify-content-center align-items-center vh-100 bg-light">
+            <div class="col-12 col-md-8 col-lg-3">
+                <div class="card shadow-sm rounded-5">
+                    <div class="card-body p-md-4">
+                    <div class="text-center mb-5">
+                        <img class="" src="../assets/tracker-index-logo.svg"
+                                height="100" alt="index-logo-img">
+                    </div>
+                        
+                <div class="display-6 mb-5 text-center">Sign In Now</div>
+                <form>
+                    <div class="mb-4">
+                        <input type="email" class="form-control form-control-lg rounded-pill shadow-sm" id="emailInput">
+                    </div>
+                    <div class="mb-4">
+                        <input type="password" class="form-control form-control-lg rounded-pill shadow-sm"
+                            id="passwordInput">
+                    </div>
+                    <div class="mb-4 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>                    
+                    <div class="d-grid mb-4 pt-5 mt-md-5">
+                        <button type="submit" class="btn btn-primary shadow-sm btn-lg rounded-pill">Sign In</button>
+                    </div>
+                    <div class="text-center">
+                        <div class="text-muted">Don't you have an account?</div>
+                    <a href="#" class="text-decoration-none">Sign Up</a>
+                    </div>
+                    
+                </form>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 
 
 <style>
-
 </style>
